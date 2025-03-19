975baa294cbf7f17389031db0f5b90d88478bf75
Kirpich-12 <ponchik34@tuta.io>
2025-03-19 13:23:49 +0300

    vse

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from dr import main
+#Kirpich aka Deros. All rights reserved.
+#======================================
 
-main()
+
+from dr import left, right
+
+
+def main():
+    usr_inp = input('1 or 2')
+    if usr_inp == 1:
+        left()
+    elif usr_inp == 2:
+        right()
+
+if __name__ == '__main__':
+    main()
